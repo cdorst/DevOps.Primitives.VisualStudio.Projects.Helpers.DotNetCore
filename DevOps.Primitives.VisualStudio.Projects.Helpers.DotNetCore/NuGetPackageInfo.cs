c8a143fd8fc30c44b73a3ec6cf4c109c4f342faa
Christopher Dorst <chris@dorst.email>
2018-03-07 05:36:21 -0600

    Breaking change to NuGetPackageInfo; add/require additional properties

--- DevOps.Primitives.VisualStudio.Projects.Helpers.DotNetCore/NuGetPackageInfo.cs
+++ DevOps.Primitives.VisualStudio.Projects.Helpers.DotNetCore/NuGetPackageInfo.cs
@@ -3,19 +3,29 @@
     public class NuGetPackageInfo
     {
         public NuGetPackageInfo() { }
-        public NuGetPackageInfo(string authors, string description, string version, bool? generatePackageOnBuild = false, string packageId = null)
+        public NuGetPackageInfo(string version, string authors, string copyright, string description, string packageIconUrl, string packageLicenseUrl, string packageProjectUrl, string repositoryUrl, bool? generatePackageOnBuild = false, string packageId = null)
         {
             Authors = authors;
+            Copyright = copyright;
             Description = description;
             GeneratePackageOnBuild = generatePackageOnBuild;
+            PackageIconUrl = packageIconUrl;
             PackageId = packageId;
+            PackageLicenseUrl = packageLicenseUrl;
+            PackageProjectUrl = packageProjectUrl;
+            RepositoryUrl = repositoryUrl;
             Version = version;
         }
 
         public string Authors { get; set; }
+        public string Copyright { get; set; }
         public string Description { get; set; }
         public bool? GeneratePackageOnBuild { get; set; }
+        public string PackageIconUrl { get; set; }
         public string PackageId { get; set; }
+        public string PackageLicenseUrl { get; set; }
+        public string PackageProjectUrl { get; set; }
+        public string RepositoryUrl { get; set; }
         public string Version { get; set; }
     }
 }
